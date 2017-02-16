3409b06553e5d0a89498d61a6c947aa051d47c8d
kudakov_r <kudakov_r@Kudakov-R.dept07>
2017-02-16 16:15:44 +0300

    Update filter

--- trunk/V8LogScanner/src/org/v8LogScanner/commonly/Filter.java
+++ trunk/V8LogScanner/src/org/v8LogScanner/commonly/Filter.java
@@ -3,6 +3,7 @@ package org.v8LogScanner.commonly;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class Filter<T1> implements Iterable<T1>, Serializable{
 	
@@ -12,7 +13,7 @@ public class Filter<T1> implements Iterable<T1>, Serializable{
 	
 	public ComparisonTypes comparisonType = ComparisonTypes.equal;
 	
-	private ArrayList<T1> elements = new ArrayList<T1>();
+	private List<T1> elements = new ArrayList<T1>();
 	
 	public Filter<T1> add(T1 val){
 		elements.add(val);
@@ -41,6 +42,14 @@ public class Filter<T1> implements Iterable<T1>, Serializable{
 	
 	public Iterator<T1> iterator() {
 		return elements.iterator();
+	}
+	
+	public List<T1> getElements() {
+	  return elements;
+	}
+	
+	public void setElements(List<T1> elements) {
+	  this.elements = elements;
 	}
 }
 
