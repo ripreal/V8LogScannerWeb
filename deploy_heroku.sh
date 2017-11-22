1cb541c8bf620dcaebf625351571d52bcf1178ae
Ripreal <ripreal1@yandex.ru>
2017-11-22 18:37:03 +0300

    Update deploy_heroku.sh

--- deploy_heroku.sh
+++ deploy_heroku.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
 heroku plugins:install heroku-container-registry
-docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
+docker login --username=ripreal --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
+
+docker login --username=_ --password=${YOUR_TOKEN} registry.heroku.com
